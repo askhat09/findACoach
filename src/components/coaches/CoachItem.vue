4ff10354ff5d817032388844d3bd855c3e01c146
Askhat <askhat.s@halykmarket.com>
2023-09-03 21:39:18 +0600

    adding a couple base ui components

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -1,33 +1,34 @@
 <template>
-	<li>
-		<h3>{{ fullName }}</h3>
-		<h4>${{ rate }}/hour</h4>
-		<div><span v-for="area in areas" :key="area">
-			{{ area }}
-		</span></div>
-		<div class="actions">
-			<router-link :to="coachContactLink">Contact</router-link>
-			<router-link :to="coachDetailsLink">View Details</router-link>
-		</div>
-	</li>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <base-badge v-for="area in areas" :key="area" :type="area" :title="area">
+      {{ area }}
+    </base-badge>
+    <div class="actions">
+      <base-button link mode="outline" :to="coachContactLink"
+        >Contact</base-button
+      >
+      <base-button link :to="coachDetailsLink">View Details</base-button>
+    </div>
+  </li>
 </template>
 
 <script>
 export default {
-	props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
-	computed: {
-		fullName () {
-			return `${this.firstName} ${this.lastName}`
-		},
-		coachContactLink () {
-			return `${this.$route.path}/${this.id}/contact`
-		}
-		,
-		coachDetailsLink () {
-			return `${this.$route.path}/${this.id}`
-		}
-	}
-}
+  props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    coachContactLink() {
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+    coachDetailsLink() {
+      return `${this.$route.path}/${this.id}`;
+    },
+  },
+};
 </script>
 
 <style scoped>
