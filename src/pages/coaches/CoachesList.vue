91b9158aa6b9a54a5b3a59bc637a77c4bdb2d2f5
Askhat <askhat.s@halykmarket.com>
2023-09-03 14:52:18 +0600

    working on the coaches list and items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,23 +6,48 @@
       <router-link to="/register">Register as Coash</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-				{{ coach.firstName }}
-			</li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+				:last-name="coach.lastName"
+				:rate="coach.hourlyRate"
+				:areas="coach.areas"
+      />
     </ul>
-		<h3 v-else>Coaches not found.</h3>
+    <h3 v-else>Coaches not found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: {
+    CoachItem,
+  },
+
   computed: {
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
     },
-		filteredCoaches() {
-			return this.$store.getters['coaches/coaches'];
-		}
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
